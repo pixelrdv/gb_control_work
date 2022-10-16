d2c50fb7275589cc73df426c9733b1b731513594
Dmitriy Rogachev <i@rdv90.ru>
2022-10-16 21:24:31 +0300

    add algorithm

---
+++ control_work.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+def get_list() -> list:
+    pass
+
+
+def sort_list(l: list) -> list:
+    pass
+
+
+if __name__ == '__main__':
+    result = list()
+
+    strings_list = get_list()
+    result = sort_list()
+    print(result)
